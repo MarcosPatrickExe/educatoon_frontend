ece21f6e5c44ee402c2693b5a7191f9ba63880a1
MarcosPatrickExe <marcospatrick039474@gmail.com>
2023-06-21 21:59:44 -0300

    Select Category page is almost complete

--- src/pages/category/Category.tsx
+++ src/pages/category/Category.tsx
@@ -2,12 +2,41 @@ import groundGrass from '../../assets/ground_grass.svg';
 import wave from '../../assets/wave.svg';
 import './Category.css';
 import cloud from '../../assets/cloud.png';
+import Colors from '../../components/Utils';
+
 
 
 export default function Category(){
 
     return (
         <>
+            <h3 className="title-logo">Educatoon</h3>
+
+            <div className="box-icons">
+                <div className="find-icon">
+                    <i className="fa fa-search"></i>
+                    {/* <img src={ require("../../assets/search-icon.png")} alt="search icon"></img> */}
+                </div>
+                <div className="setting-icon">
+                    <i className="fa fa-cog"></i>
+                    {/* <img src={ require("../../assets/setting-icon.png")} alt="setting icon"></img> */}
+                </div>
+            </div>
+           
+
+           <div className="box-top-center-text">
+                <div className="div-top-center-text" >
+                    <p> 
+                        <span style={{color: '#F3B700'}}>Escolha</span> sua jornada
+                    </p>
+                </div>
+
+                <div className="div-bottom-center-text" style={{backgroundColor: Colors.YELLOW }}>
+                    <p> Iniciar </p>
+                </div>
+           </div>
+
+
             <img className="cloud-left" src={cloud} alt="cloud" />
 
             <img className="cloud-right" src={cloud} alt="cloud" />
@@ -22,3 +51,8 @@ export default function Category(){
         </>
     );
 }
+
+
+// git pull origin other-branch
+
+// git push origin localBranchName:remoteBranchName
