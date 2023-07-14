597555470105a61b614af8d585a955d36b8460d7
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-07-14 19:02:14 +0000

    Adding svgs comics to 'tirinha.tsx'

--- src/pages/tirinha/Tirinha.tsx
+++ src/pages/tirinha/Tirinha.tsx
@@ -1,34 +1,31 @@
-import React, { ButtonHTMLAttributes, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import './tirinha.css';
-import pg1 from "../../assets/tirinha/pg-1.png";
-import pg2 from "../../assets/tirinha/pg-2.jpg";
-import pg3 from "../../assets/tirinha/pg-3.jpg";
-import pg4 from "../../assets/tirinha/pg-4.jpg";
-import pg5 from "../../assets/tirinha/pg-5.jpg";
-import pg6 from "../../assets/tirinha/pg-6.jpg";
-import pg7 from "../../assets/tirinha/pg-7.jpg";
+import React, { ButtonHTMLAttributes, useRef } from "react";
+import { Link } from "react-router-dom";
+import "./tirinha.css";
+import pg1 from "../../assets/tirinha/1.svg";
+import pg2 from "../../assets/tirinha/2.svg";
+import pg3 from "../../assets/tirinha/3.svg";
+import pg4 from "../../assets/tirinha/4.svg";
+import pg5 from "../../assets/tirinha/5.svg";
+import pg6 from "../../assets/tirinha/6.svg";
+import pg7 from "../../assets/tirinha/7.svg";
 import pg8 from "../../assets/tirinha/pg-8-2.jpg";
 import pg9 from "../../assets/tirinha/pg-9.jpg";
-
-
-
+import pg10 from "../../assets/tirinha/10.svg";
+import pg11 from "../../assets/tirinha/11.svg";
+import pg12 from "../../assets/tirinha/12.svg";
+import pg13 from "../../assets/tirinha/13.svg";
 function Tirinha() {
-
   const pg8ref = React.createRef<HTMLImageElement>();
   const pg9ref = React.createRef<HTMLImageElement>();
 
-
-  const moveToPreviousComic = ()=>{
-        let imgElement = pg8ref.current as HTMLImageElement;
-        imgElement.scrollIntoView();
-  }
-  const moveToNextComic = ()=>{
-        let imgElement2 = pg9ref.current as HTMLImageElement;
-        imgElement2.scrollIntoView();
-  }
-
-
+  const moveToPreviousComic = () => {
+    let imgElement = pg8ref.current as HTMLImageElement;
+    imgElement.scrollIntoView();
+  };
+  const moveToNextComic = () => {
+    let imgElement2 = pg9ref.current as HTMLImageElement;
+    imgElement2.scrollIntoView();
+  };
 
   return (
     <div className="background">
@@ -43,25 +40,27 @@ function Tirinha() {
         <img ref={pg8ref} src={pg8} alt="" />
 
         <div className="caixa-interacao">
-            <div className="imagem-interacao">
-            </div>
+          <div className="imagem-interacao"></div>
 
-            <div className="texto-interacao">
-                <p>RAUL OLHA PARA AS BOLINHAS DE GUDE EM SUAS MÃOS, E DIZ QUE TEM O TOTAL DE:</p>
-                <div className="btns-interacao">
-                    <button onClick={ moveToPreviousComic }> 5 </button>
-                    <button onClick={ moveToNextComic }> 9 </button>
-                    <button onClick={ moveToPreviousComic }> 7 </button>
-                    <button onClick={ moveToPreviousComic }> 10 </button>
-                </div>
+          <div className="texto-interacao">
+            <p>
+              RAUL OLHA PARA AS BOLINHAS DE GUDE EM SUAS MÃOS, E DIZ QUE TEM O
+              TOTAL DE:
+            </p>
+            <div className="btns-interacao">
+              <button onClick={moveToPreviousComic}> 5 </button>
+              <button onClick={moveToNextComic}> 9 </button>
+              <button onClick={moveToPreviousComic}> 7 </button>
+              <button onClick={moveToPreviousComic}> 10 </button>
             </div>
+          </div>
         </div>
 
         <img ref={pg9ref} src={pg9} alt="page final" />
-
+       
       </div>
     </div>
-  )
+  );
 }
 
 export default Tirinha;
